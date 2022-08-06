a301027c7460a8b97945796324138baf5ebcdf92
marvel0us-admin <487703461@qq.com>
2022-08-06 22:37:56 +0800

    create program

---
+++ src/App.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, nativeImage, Tray, Menu } = require('electron');
+const path = require('path')
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1400,
+        height: 900,
+        resizable: false,
+        icon: nativeImage.createFromPath('../assets/img/favicon.ico'),
+        webPreferences: {
+            preload: path.join(__dirname, './preload.js'),
+        }
+    })
+    mainWindow.loadFile("./views/index.html");
+}
+
+
+let tray
+const trayFun = (icon) => {
+    tray = new Tray(icon)
+    tray.setToolTip('To dos.')
+    tray.setTitle('To dos.')
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    });
+
+    const icon = nativeImage.createFromPath('../assets/img/favicon.ico')
+    trayFun(icon)
+})
+
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
